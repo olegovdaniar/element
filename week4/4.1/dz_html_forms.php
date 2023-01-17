7ce6300384e5a8970304fb9917f249e92a98158c
olegovdaniar <66049565+olegovdaniar@users.noreply.github.com>
2023-01-17 16:11:11 +0600

    Create dz_html_forms.php

---
+++ week4/4.1/dz_html_forms.php
@@ -0,0 +1,7 @@
+<form method='get' action='dz_html_postget.php' id = form1>
+    <input name = 'age' placeholder="age">
+    <input name = 'nation' placeholder="nation">
+    <input name = 'country' placeholder="country">
+</form>
+<button type="submit" form="form1" value="Submit">Submit</button>
+
